df572e9d2578005fc233faced15896e5c34775ec
osmandi <osmandi.github@gmail.com>
2018-02-09 12:28:01 -0400

    Agregando componentes

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  footer.footer
+    .container
+      .content.has-text-centered
+        p 🖥With ❤ By @osmandi 
+</template>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 😈 Platzi Music
+          .nav-right.nav-menu
+    .hero-body
+      .contaniner.has-text-centered
+        h1.tittle Platzi Music
+        h2.subtittle Canciones que están en Vue
+</template>

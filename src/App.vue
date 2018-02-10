4cac3437c89a861ede100c2638de80bb21904138
osmandi <osmandi.github@gmail.com>
2018-02-10 19:09:14 -0400

    Hasta comunicación de hijos a padres

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,67 @@
 <template lang="pug">
   #app
     pm-header
-    section.section
-      nav.nav.has-shadow
+
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
         .container
-          .columns
-            .column
-            input.input.is-large(
-              type="text", 
-              placeholder="Buscar canción", 
-              v-model="searchQuery"
-            )
-            a.button.is-info.is-large(@click="search") Buscar
-            a.button.is-danger.is-large &times;
+          input.input.is-large(
+            type="text", 
+            placeholder="Buscar canción 2", 
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
       .container
         p
           small {{ searchMessage  }}
 
-        .container.results
-          .columns(v-for="t in tracks") 
-            | {{ t.name }} - {{ t.artists[0] }}
-            .column
+      .container.results
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks") 
+            pm-track(
+              :class= "{ 'is-active': t.id == selectedTrack }",
+              :track="t", 
+              @select="setSelectedTrack"
+            )
+            
     pm-footer
 </template>
 
 
 <script>
-import trackService from './services/track'
-import PmFooter from './components/layout/Footer.vue'
-import PmHeader from './components/layout/Header.vue'
+import trackService from '@/services/track'
+import PmFooter from '@/components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
 
+import PmTrack from '@/components/Track.vue'
+import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
-  components: { PmFooter, PmHeader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader },
   data () {
     return {
         searchQuery: '',
-        tracks: []
+        tracks: [],
+        isLoading: false,
+        selectedTrack: ''
     }
  },
  methods: {
     search () {
       if (!this.searchQuery) { return }
+      this.isLoading = true
      // this.tracks = tracks
       trackService.search(this.searchQuery)
         .then(res => {
           this.tracks = res.tracks.items
+          this.isLoading = false
         })
+    },
+    setSelectedTrack(id){
+      this.selectedTrack= id
     }
   },
   computed: {
@@ -60,7 +74,12 @@ export default {
 
 <style lang="scss">
   @import './scss/main.scss';
-  .results{
-    margin-top:50px
-  }
+  
+  .results {
+    margin-top:50px;
+  },
+  
+  .is-active{
+    border: 3px #23d160 solid;
+  }  
 </style> 
---
+++ src/components/shared/Loader.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .container
+    .content.has-text-centered
+      h1 Cargando ...
+</template>
+
+<style lang="scss" scopped>
+  .content {
+    padding: 20px;
+    mind-height: 40vh;
+  }
+</style>

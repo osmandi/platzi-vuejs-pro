14eee4f542bb2d1cacd9430ae9309e20ec61b56a
osmandi <osmandi.github@gmail.com>
2018-02-13 15:34:08 -0400

    Modifiers

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -10,7 +10,8 @@
           input.input.is-large.is-rounded(
            type="text", 
            placeholder="Buscar canciones",
-           v-model="searchQuery"
+           v-model="searchQuery",
+           @keyup.enter="search"
           )          
           a.button.is-info.is-large(@click="search", id="b-buscar") Buscar          
           a.button.is-danger.is-large &times;
